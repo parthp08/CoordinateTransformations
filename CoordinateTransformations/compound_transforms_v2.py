e292a8ae9b0e9a4f9741a0ac344a13289ee8ddd9
Parth Viradiya <36721553+parthp08@users.noreply.github.com>
2020-02-17 17:35:30 -0500

    Update compound_transforms_v2.py

--- CoordinateTransformations/compound_transforms_v2.py
+++ CoordinateTransformations/compound_transforms_v2.py
@@ -8,10 +8,8 @@ def compound_transforms_v2(T_arr):
 
     TODO
     """
-    T = np.matrix(np.zeros((4,4)))
-    T[3,3] = 1
-
-    for i in range(1, len(T_arr)-1):
+    
+    for i in range(1, len(T_arr)):
         if i == 1:
             T = compound_transforms(T_arr[i-1], T_arr[i])
         else:

7be36b8c4e78ba7725e50b78a1a12ba185dab013
Parth Viradiya <36721553+parthp08@users.noreply.github.com>
2020-02-17 17:53:20 -0500

    corrected returns

--- CoordinateTransformations/axis_angle2transform.py
+++ CoordinateTransformations/axis_angle2transform.py
@@ -1,6 +1,7 @@
 import numpy as np
 from .axis_angle2rotation import axis_angle2rotation
 from .rotation2transform import rotation2transform
+from .RP2T import RP2T
 
 def axis_angle2transform(vector_, theta, unit='rad', pass_through_origin=True, pass_point=np.zeros((3,1))):
     """
@@ -9,6 +10,10 @@ def axis_angle2transform(vector_, theta, unit='rad', pass_through_origin=True, p
     => is translation case applies here ??
     """
 
-    R = axis_angle2rotation(vector_, theta, unit=unit, pass_through_origin=pass_through_origin, pass_point=pass_point)
-    T = rotation2transform(R)
-    return T
+    if not pass_through_origin:
+        R, P = axis_angle2rotation(vector_, theta, unit=unit, pass_through_origin=pass_through_origin, pass_point=pass_point)
+        T = RP2T(R,P)
+    else:
+        R = axis_angle2rotation(vector_, theta, unit=unit, pass_through_origin=pass_through_origin, pass_point=pass_point)
+        T = rotation2transform(R)
+        return T

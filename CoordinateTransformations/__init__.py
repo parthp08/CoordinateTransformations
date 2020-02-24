4b90b609823b8d931ed896f986dd7badb3fc0d20
Parth Viradiya <36721553+parthp08@users.noreply.github.com>
2020-02-24 17:23:24 -0500

    Update __init__.py

--- CoordinateTransformations/__init__.py
+++ CoordinateTransformations/__init__.py
@@ -4,39 +4,44 @@
 from .R_x import R_x
 from .R_y import R_y
 from .R_z import R_z
+from .rotation_operator import rotation_operator as R_operator
+from .compound_rotations import compound_rotations as R_compound
+from .compound_rotations_v2 import compound_rotations_v2 as R_compound_v2
+from .inverse_rotation import inverse_rotation as R_inverse
 
 # Homogeneous Transformation base functions
 from .T_x import T_x
 from .T_y import T_y
 from .T_z import T_z
-from .transform_operator import transform_operator
-from .compound_transforms import compound_transforms
-from .compound_transforms_v2 import compound_transforms_v2
-from .inverse_transform import inverse_transform
+from .transform_operator import transform_operator as T_operator
+from .compound_transforms import compound_transforms  as T_compound
+from .compound_transforms_v2 import compound_transforms_v2 as T_compound_v2
+from .inverse_transform import inverse_transform as T_inverse
 
 # Translation functions
 from .pure_translation import pure_translation
-from .T_translation import T_translation
+from .T_translation import T_translation as P2T
 from .RP2T import RP2T
+from .T2RP import T2RP
 
 # from Rotation Matrix to .... functions
-from .rotation2euler import rotation2euler
-from .rotation2transform import rotation2transform
-from .rotation2axis_angle import rotation2axis_angle
+from .rotation2euler import rotation2euler as R2Euler
+from .rotation2transform import rotation2transform as R2T
+from .rotation2axis_angle import rotation2axis_angle as R2AxAng
 
 # from Euler angles to .... functions
-from .euler2rotation import euler2rotation
-from .euler2transform import euler2transform
+from .euler2rotation import euler2rotation as Euler2R
+from .euler2transform import euler2transform as Euler2T
 
 # from Homogeneous Transformations to .... functions
-from .transform2rotation import transform2rotation
-from .transform2translation_vec import transform2translation_vec
-from .transform2euler import transform2euler
-from .transform2axis_angle import transform2axis_angle
+from .transform2rotation import transform2rotation as T2R
+from .transform2translation_vec import transform2translation_vec as T2P
+from .transform2euler import transform2euler as T2Euler
+from .transform2axis_angle import transform2axis_angle as T2AxAng
 
 # from Axis-Angle to .... functions
-from .axis_angle2rotation import axis_angle2rotation
-from .axis_angle2transform import axis_angle2transform
+from .axis_angle2rotation import axis_angle2rotation as AxAng2R
+from .axis_angle2transform import axis_angle2transform as AxAng2T
 
 # Quaternions base functions
 
